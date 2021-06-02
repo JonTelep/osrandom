0a4eb7b1aaef69150f96cba56dd9032f3455efb2
jontelep <telepjonathan@gmail.com>
2021-06-02 02:23:10 -0400

    Finished adding all bosses as an object with name, isgwd, iswildy, ismulti, israids, youtube, osrswiki attributes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,383 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+import Donation from './components/donation';
+
+
+const bosses = [
+  {
+    name: 'Kree\'arra',
+    isGWD: true,
+    isWILDY: false,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/Xvbmz3Itfx8',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Kree%27arra/Strategies'
+  },
+  {
+    name: 'General Graardor',
+    isGWD: true,
+    isWILDY: false,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/JK3_IDlQzUI',
+    osrswiki: 'https://oldschool.runescape.wiki/w/General_Graardor/Strategies'
+  },
+  {
+    name: 'K\'ril Tsutsaroth',
+    isGWD: true,
+    isWILDY: false,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/bIVvp50wrNA',
+    osrswiki: 'https://oldschool.runescape.wiki/w/K%27ril_Tsutsaroth/Strategies'
+  },
+  {
+    name: 'Commander Zilyana',
+    isGWD: true,
+    isWILDY: false,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/jWaIYzH5raE',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Commander_Zilyana/Strategies'
+  },
+  {
+    name: 'Callisto',
+    isGWD: false,
+    isWILDY: true,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/M3HB0g1vJsY',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Callisto/Strategies'
+  },
+  {
+    name: 'Chaos Elemental',
+    isGWD: false,
+    isWILDY: true,
+    isMulti: false,
+    isRaids: false,
+    youtube: 'https://youtu.be/OZLUJA_y7hw',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Chaos_Elemental/Strategies'
+  },
+  {
+    name: 'Crazy Archaeologist',
+    isGWD: false,
+    isWILDY: true,
+    isMulti: false,
+    isRaids: false,
+    youtube: 'https://youtu.be/lujbZUGMz7g',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Crazy_archaeologist/Strategies'
+  },
+  {
+    name: 'King Black Dragon',
+    isGWD: false,
+    isWILDY: true,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/UVH7j_5xP6g',
+    osrswiki: 'https://oldschool.runescape.wiki/w/King_Black_Dragon/Strategies'
+  },
+  {
+    name: 'Scorpio',
+    isGWD: false,
+    isWILDY: true,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/mMS20r5xEyE',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Scorpia/Strategies'
+  },
+  {
+    name: 'Venenatis',
+    isGWD: false,
+    isWILDY: true,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/KvlkjdOB4tg',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Venenatis/Strategies'
+  },
+  {
+    name: 'Vet\'ion',
+    isGWD: false,
+    isWILDY: true,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/2yXc9bKdvOE',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Vet%27ion/Strategies'
+  },
+  {
+    name: 'Obor',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: false,
+    isRaids: false,
+    youtube: 'https://youtu.be/4VXoE6-5cA4',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Obor'
+  },
+  {
+    name: 'Brophyta',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: false,
+    isRaids: false,
+    youtube: 'https://youtu.be/BgbFVxap1Ec',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Bryophyta'
+  },
+  {
+    name: 'Tempoross',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: false,
+    isRaids: false,
+    youtube: 'https://youtu.be/R4Ws-xxOS-A',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Tempoross/Strategies'
+  },
+  {
+    name: 'Wintertodt',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: false,
+    isRaids: false,
+    youtube: 'https://youtu.be/5mOXlO0DzU8',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Wintertodt/Strategies'
+  },
+  {
+    name: 'Zalcano',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: false,
+    isRaids: false,
+    youtube: 'https://youtu.be/aP56mYJB_EY',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Zalcano/Strategies'
+  },
+  {
+    name: 'Chambers of Xeric',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: false,
+    isRaids: true,
+    youtube: 'https://youtu.be/wlmYhW6qmmw',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Chambers_of_Xeric/Strategies'
+  },
+  {
+    name: 'Chambers of Xeric (Challenge Mode)',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: false,
+    isRaids: true,
+    youtube: 'https://youtu.be/wlmYhW6qmmw',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Chambers_of_Xeric/Strategies'
+  },
+  {
+    name: 'Theater of Blood',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: false,
+    isRaids: true,
+    youtube: 'https://youtu.be/7hzX4552lso',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Theatre_of_Blood/Strategies'
+  },
+  {
+    name: 'Theater of Blood (Hard Mode)',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: false,
+    isRaids: true,
+    youtube: 'https://youtu.be/7hzX4552lso',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Theatre_of_Blood/Strategies'
+  },
+  {
+    name: 'Giant Mole',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/Tl1aNgoO2hs',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Giant_Mole/Strategies'
+  },
+  {
+    name: 'Deranged Archaeologist',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/yG0h2bKOzuY',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Deranged_archaeologist/Strategies'
+  },
+  {
+    name: 'Dagannoth Kings',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/pfjvjt95Fow',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Dagannoth_Kings/Strategies'
+  },
+  {
+    name: 'Sarachnis',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/Ce_jV9iJNiE',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Sarachnis/Strategies'
+  },
+  {
+    name: 'Kalphite Queen',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/qmFvRtJkKHk',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Kalphite_Queen/Strategies'
+  },
+  {
+    name: 'Zulrah',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: false,
+    isRaids: false,
+    youtube: 'https://youtu.be/CZFzB71k6wU',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Zulrah/Strategies'
+  },
+  {
+    name: 'Vorkath',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: false,
+    isRaids: false,
+    youtube: 'https://youtu.be/d6kR7eBSw1k',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Vorkath/Strategies'
+  },
+  {
+    name: 'Corporeal Beast',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/3ZNGpC90zEI',
+    osrswiki: 'https://oldschool.runescape.wiki/w/Corporeal_Beast/Strategies'
+  },
+  {
+    name: 'The Nightmare',
+    isGWD: false,
+    isWILDY: false,
+    isMulti: true,
+    isRaids: false,
+    youtube: 'https://youtu.be/tihXu1-KYVk',
+    osrswiki: 'https://oldschool.runescape.wiki/w/The_Nightmare/Strategies'
+  }
+]
+
+
+const skills = [
+  'Agility',
+  'Attack',
+  'Strength',
+  'Defence',
+  'Ranged',
+  'Prayer',
+  'Magic',
+  'Runecraft',
+  'Hitpoints',
+  'Crafting',
+  'Mining',
+  'Smithing',
+  'Fishing',
+  'Cooking',
+  'Firemaking',
+  'Wooducutting',
+  'Herblore',
+  'Thieving',
+  'Fletching',
+  'Slayer',
+  'Farming',
+  'Construction',
+  'Hunter'
+];
+
+
 
 function App() {
+  //GLOBAL
+  const [bossesSelected, setBossesSelected] = useState(false);
+
+  //BOSSES
+  const [bossName, setBossName] = useState('');
+  const [bossIsGWD, setBossIsGWD] = useState('');
+  const [bossIsWildy, setBossIsWildy] = useState('');
+  const [bossIsMulti, setBossIsMulti] = useState('');
+  const [bossIsRaids, setBossIsRaids] = useState('');
+  const [bossYoutube, setBossYoutube] = useState('');
+  const [bossWiki, setBossWiki] = useState('');
+
+
+  //SKILLS
+  const [randomSkill, setRandomSkill] = useState('');
+
+
+const getRandomNumber = (max) => {
+  return Math.floor(Math.random() * max);
+}
+
+
+const SelectRandomBoss = () => {
+  setBossesSelected(true);
+  const randomNum = getRandomNumber(bosses.length);
+  setBossName(bosses[randomNum].name);
+  setBossIsGWD(bosses[randomNum].isGWD);
+  setBossIsWildy(bosses[randomNum].isWILDY);
+  setBossIsMulti(bosses[randomNum].isMulti);
+  setBossIsRaids(bosses[randomNum].isRaids);
+  setBossYoutube(bosses[randomNum].youtube);
+  setBossWiki(bosses[randomNum].osrswiki);
+}
+
+const SelectRandomSkill = () => {
+  const randomNum = getRandomNumber(skills.length);
+  setRandomSkill(skills[randomNum]);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="App">
+        <header className="App-header">
+          <div className="ui vertical segment">
+            <button className="ui secondary button" onClick={SelectRandomBoss}>Pick your random boss</button>
+            {bossesSelected ?
+            <div className="ui piled segments"> 
+                <div className="ui inverted segment">
+                    <h2>{bossName} </h2>
+                </div>         
+                <div className="ui inverted segment">
+                    { bossIsWildy ? ` is in the wildy.` : ` is not in the wildy.`}
+                </div>
+                <div className="ui inverted segment">
+                    { bossIsGWD ? ` is in the God Wars Dungeon.` : ` is not in the God Wars Dungeon.`}
+                </div>
+                <div className="ui inverted segment">
+                    { bossIsMulti ? ` is in Multi.` : ` is not Multi.`}
+                </div>
+                <div className="ui inverted segment">
+                    { bossIsRaids ? ` is a Raid.` : ` is not a Raid.`}
+                </div>
+                <div className="ui inverted segment">
+                  <a href={bossWiki}>OSRS Wiki Strategy</a>
+                </div>
+                <div className="ui inverted segment">
+                  <a href={bossYoutube}>A youtube video of a guide</a>
+                </div>
+            </div> : ''
+          }
+          </div>
+          {/*
+          <div>
+            <button className="ui secondary button" onClick={SelectRandomSkill}>Pick your random skill</button>
+            The skill you should train is you are doing is: {randomSkill}
+          </div>
+           <Donation/> */}
+        </header>
+      </div>
+
   );
 }
 
---
+++ src/components/donation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Donation = () => {
+    return(
+        <div>
+            <h1>If you want to support me you can donate here:</h1>
+                <ul>
+                    <li> <a href="https://www.patreon.com/Telep">Patreon</a></li>
+                    <li>Ethereum - 0xe84edd441dcADE214dE3973c31e1fc930006e588</li>
+                    <li>Bitcoin - </li>
+                    <li>Doge</li>
+                    <li></li>
+                </ul>
+        </div>
+    );
+
+}
+
+export default Donation;
